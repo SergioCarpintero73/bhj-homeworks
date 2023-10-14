905acd0f6ad14218f53aa1b560e4619de986a4b3
SergioCarpintero73 <92789674+SergioCarpintero73@users.noreply.github.com>
2023-10-14 20:14:20 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,29 +1,10 @@
+const menuList = document.querySelectorAll('.menu__link');
 
-
-
-const xxx = document.querySelectorAll(".menu__item");
-const arr = Array.from(xxx);
-
-let item_on;
-arr.forEach((item) => {
-    if (item.querySelector('.menu_sub')) {
-        item.querySelector('.menu__link').onclick = () => {
-            item_on = arr.indexOf(item);
-            roll();
-            return false;
+for (const menu of menuList) {
+    menu.addEventListener( 'click', (event) => {
+        if (menu.nextElementSibling && menu.nextElementSibling.className.includes('menu_sub')) {
+            menu.nextElementSibling.classList.toggle('menu_active');
+            event.preventDefault();
         }
-    }
-})
-
-function roll() {
-    for (item of arr) {
-        if (item.querySelector('.menu_sub') && arr.indexOf(item) === item_on) {
-            item.querySelector('.menu_sub').classList.toggle('menu_active')
-        } else if (item.querySelector('.menu_sub') && arr.indexOf(item) !== item_on) {
-            item.querySelector('.menu_sub').classList.toggle('menu_active', false)
-        }
-    }
+    })
 }
-
-
-

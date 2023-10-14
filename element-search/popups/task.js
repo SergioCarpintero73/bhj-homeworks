719f8c679d29b13d27954013b7c757ae772afe69
SergioCarpintero73 <92789674+SergioCarpintero73@users.noreply.github.com>
2023-10-14 20:14:57 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,20 @@
+const popMain = document.getElementById('modal_main');
+const popSuccess = document.getElementById('modal_success');
+const popClose = document.querySelectorAll('.modal__content div');
+const popBtn = document.querySelector('.modal__content a');
 
 
-const mod = document.getElementById('modal_main');
-const mod2 = document.getElementById('modal_success');
-mod.className +=" modal_active";
+popMain.classList.add('modal_active');
 
-const xxx = document.querySelectorAll(".modal__close_times");
-xxx[0].onclick = () => mod.className = "modal";
-xxx[1].onclick = () => mod2.className = "modal";
+popBtn.onclick = () => {
+    popSuccess.classList.add('modal_active');
+    popMain.classList.remove('modal_active');
+}
 
-document.querySelector(".btn_danger").onclick = () => {
-    mod.className = "modal";
-    mod2.className +=" modal_active";
+for (const button of popClose) {
+    button.addEventListener('click', () => {
+        popMain.classList.remove('modal_active');
+        popSuccess.classList.remove('modal_active');
+
+    })
 }

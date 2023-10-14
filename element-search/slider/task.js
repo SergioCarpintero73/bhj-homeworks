9e012d3c6ea7126904b52f98dede39012b1a8ecc
SergioCarpintero73 <92789674+SergioCarpintero73@users.noreply.github.com>
2023-10-14 20:35:02 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,59 @@
+const images = Array.from(document.querySelectorAll('.slider__item'));
+const arrows = document.querySelectorAll('.slider__arrow');
+const dots = Array.from(document.querySelectorAll('.slider__dot'));
+const arrowNext = arrows[1]
+const arrowPrev = arrows[0]
+const a = Array.from(images).findIndex(img => img.className.includes('slider__item_active'))
+dots[a].classList.add('slider__dot_active')
 
 
-const xxx = document.querySelectorAll(".slider__item");
-const dots = Array.from(document.querySelectorAll(".slider__dot"));
 
-document.querySelector(".slider__arrow_next").onclick = () => runSlide('1');
-document.querySelector(".slider__arrow_prev").onclick = () => runSlide('2');
-let num_img = 0;
-
-dots.forEach((item) => {
-    item.onclick = () => {
-        item_on = dots.indexOf(item);
-        console.log(item_on);
-        runDots(item_on);
+function setActiveImage (index) {
+    const active = function () {
+        const activeImage = (item) => item.classList.contains('slider__item_active');
+        return images.findIndex(activeImage);
+    };
+    const activeIndex = active()
+    images[activeIndex].classList.remove('slider__item_active');
+    dots[activeIndex].classList.remove('slider__dot_active');
+    if (index >= images.length) {
+        index = 0;
+        images[index].classList.add('slider__item_active');
+        dots[index].classList.add('slider__dot_active');
     }
-})
-
-function runDots(num) {
-    xxx[num_img].classList.toggle('slider__item_active');
-    dots[num_img].classList.toggle('slider__dot_active');
-    num_img = num;
-    xxx[num_img].classList.toggle('slider__item_active');
-    dots[num_img].classList.toggle('slider__dot_active');
+    if (index < 0) {
+        index = (images.length - 1);
+        images[index].classList.add('slider__item_active');
+        dots[index].classList.add('slider__dot_active');
+    } else {
+        images[index].classList.add('slider__item_active');
+        dots[index].classList.add('slider__dot_active');
+    }
 }
 
-function runSlide(num) {
-    xxx[num_img].classList.toggle('slider__item_active');
-    dots[num_img].classList.toggle('slider__dot_active');
-    if (num === "1") {
-        num_img += 1;
-        if (num_img === xxx.length) num_img = 0;
-    } else {
-        num_img -= 1;
-        if (num_img < 0) num_img = xxx.length - 1;
-    }
-    xxx[num_img].classList.toggle('slider__item_active');
-    dots[num_img].classList.toggle('slider__dot_active');
+
+arrowNext.addEventListener('click', () => {
+    let activeIndex = Array.from(images).findIndex(img => img.className.includes('slider__item_active'));
+    setActiveImage(activeIndex + 1)
+})
+
+arrowPrev.addEventListener('click', () => {
+    let activeIndex = Array.from(images).findIndex(img => img.className.includes('slider__item_active'));
+    setActiveImage(activeIndex - 1)
+})
+
+
+for (const dot of dots) {
+    dot.addEventListener('click', () => {
+        const dotIndex = dots.indexOf(dot);
+        dots.forEach( (dot) => {
+            if (dot.className.includes('slider__dot_active')) {
+                dot.classList.remove('slider__dot_active')
+            }
+        })
+        const activeImgIndex = Array.from(images).findIndex(image => image.className.includes('slider__item_active'))
+        images[activeImgIndex].classList.remove('slider__item_active');
+        images[dotIndex].classList.add('slider__item_active');
+        dots[dotIndex].classList.add('slider__dot_active');
+    })
 }

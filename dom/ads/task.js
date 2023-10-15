12e50418a7d4ee8d8774b2706d7660c5cf6323f0
SergioCarpintero73 <92789674+SergioCarpintero73@users.noreply.github.com>
2023-10-15 12:15:00 +0300

    fourth excercise

---
+++ dom/ads/task.js
@@ -0,0 +1,30 @@
+
+
+
+ function rotating() {
+    return new Promise(resolve => {
+        let elemAds = document.querySelector('.rotator__case_active');
+        let speed = Number(elemAds.nextElementSibling.getAttribute('data-speed'));
+        let nextElem;
+        if (elemAds.nextElementSibling === document.querySelector('.rotator').lastElementChild) {
+            nextElem = document.querySelector('.rotator').firstElementChild;
+        } else {
+            nextElem = elemAds.nextElementSibling;
+        }
+        let color = nextElem.getAttribute('data-color');
+        setTimeout(function () {
+            elemAds.classList.toggle('rotator__case_active');
+            nextElem.classList.toggle('rotator__case_active');
+            nextElem.style.color = color;
+            resolve(true)
+        }, speed)
+    })
+}
+
+
+async function testAds() {
+    while (true) {
+        await rotating();
+    }
+}
+testAds();
